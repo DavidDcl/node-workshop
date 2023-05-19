64721617968138ed62f8669a69391f4052db612b
DAVID LOPES <davidlopes0208@gmail.com>
2023-05-19 12:46:24 +0200

    workshop

---
+++ index.js
@@ -0,0 +1,6 @@
+import chalk from "chalk";
+
+console.log(chalk.redBright("David"));
+console.log(chalk.bgGreen("Tristan"));
+console.log(chalk.magenta("Claire"));
+console.log(chalk.bgCyanBright("Loris"));
